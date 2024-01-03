cd62f0086ca706a251ca5c94dfecc7375579d106
kyj0503 <heroria0503@gmail.com>
2024-01-04 01:07:14 +0900

    스위치문까지 함

---
+++ input.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+    int lucky_number = 3;
+    std::cout<<"내 비밀 수를 맞추어 보세요~"<<std::endl;
+
+    int user_input;
+
+    while(1){
+        std::cout<<"입력";
+        std::cin>>user_input;
+        
+        if(lucky_number == user_input){
+            std::cout<<"맞추셨습니다."<<std::endl;
+            break;
+        } else {
+            std::cout<<"다시 생각해보세요~"<<std::endl;
+        }
+    }
+
+    return 0;
+}
+
+//cpp에서는 c의 scanf처럼 입력받을 형태를 지정해줄 필요가 없음.
